65c9ad96b85d3bec30814ce16410ee3f0d17f2c8
KeoBongg <nguyentrongquan27112003@gmail.com>
2024-05-20 14:56:43 +0700

    Update model Customer

---
+++ src/main/java/org/booking/bookingapp/model/Customer.java
@@ -0,0 +1,85 @@
+package org.booking.bookingapp.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.GenerationType.*;
+
+@Data
+@Table(name = "customer")
+@Entity(
+        name = "Customer"
+)
+public class Customer {
+    @Id
+    @SequenceGenerator(
+            name = "customer_id",
+            sequenceName = "customer_id",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = SEQUENCE,
+            generator = "customer_id"
+    )
+    @Column(
+            name = "customerId",
+            nullable = false
+    )
+    private Integer customerId;
+    @Column(
+            name = "username",
+            nullable = false,
+            updatable = false
+    )
+    private String username;
+    @Column(
+            name = "email",
+            nullable = false
+    )
+    private String email;
+    @Column(
+            name = "password",
+            nullable = false
+    )
+    private String password;
+    @Column(
+            name = "role",
+            nullable = false
+    )
+    private String role;
+    @Column(
+            name = "firstName",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
+    private String firstName;
+    @Column(
+            name = "lastName",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
+    private String lastName;
+    @Column(
+            name = "address",
+            nullable = false
+    )
+    private String address;
+    @Column(
+            name = "phoneNumber",
+            nullable = false
+    )
+    private String phoneNumber;
+    @Column(
+            name = "avatar"
+    )
+    private String avatar;
+    @Column(
+            name = "createAt",
+            nullable = false,
+            columnDefinition = "TIMESTAMP WITHOUT TIME ZONE"
+    )
+    private LocalDateTime createAt;
+
+}
--- src/main/java/org/booking/bookingapp/model/User.java
+++
@@ -1,4 +0,0 @@
-package org.booking.bookingapp.model;
-
-public class User {
-}

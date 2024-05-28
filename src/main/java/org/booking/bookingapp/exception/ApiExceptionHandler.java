44939c04c34473609aed48c019148c3ae2ced9d6
KeoBongg <nguyentrongquan27112003@gmail.com>
2024-05-28 17:14:12 +0700

    Update 500 error Exception

--- src/main/java/org/booking/bookingapp/exception/ApiExceptionHandler.java
+++ src/main/java/org/booking/bookingapp/exception/ApiExceptionHandler.java
@@ -15,14 +15,19 @@ public class ApiExceptionHandler {
 
     private final static Logger LOGGER = LoggerFactory.getLogger(ApiExceptionHandler.class);
 
-//    @ExceptionHandler(value = ApiRequestException.class)
-//    public ResponseEntity<Object> handleApiRequestException(ApiRequestException e){
-//        ApiException apiException = new ApiException(e.getMessage(),e,HttpStatus.BAD_REQUEST, ZonedDateTime.now());
-//        return new ResponseEntity<>(apiException, HttpStatus.BAD_REQUEST);
-//    }
+    @ExceptionHandler(value = ApiRequestException.class)
+    public ResponseEntity<Object> handleApiRequestException(ApiRequestException e){
+        ApiException apiException = new ApiException(e.getMessage(),e,HttpStatus.BAD_REQUEST, ZonedDateTime.now());
+        return new ResponseEntity<>(apiException, HttpStatus.BAD_REQUEST);
+    }
     @ExceptionHandler(value = NotFoundException.class)
     public ResponseEntity<Object> handleApiRequestException(NotFoundException e){
         ApiException apiException = new ApiException(e.getMessage(),e,HttpStatus.NOT_FOUND, ZonedDateTime.now());
         return new ResponseEntity<>(apiException, HttpStatus.NOT_FOUND);
+    }
+    @ExceptionHandler(value = InternalServerException.class)
+    public ResponseEntity<Object> handleApiRequestException(InternalServerException e){
+        ApiException apiException = new ApiException(e.getMessage(),e,HttpStatus.INTERNAL_SERVER_ERROR, ZonedDateTime.now());
+        return new ResponseEntity<>(apiException, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
--- src/main/java/org/booking/bookingapp/exception/ApiRequestException.java
+++ src/main/java/org/booking/bookingapp/exception/ApiRequestException.java
@@ -1,6 +1,5 @@
 package org.booking.bookingapp.exception;
 //This is we are going to throw of our code
-@Deprecated
 public class ApiRequestException extends RuntimeException{
     public ApiRequestException(String message) {
         super(message);

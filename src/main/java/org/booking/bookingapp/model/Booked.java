0ceb0ce373bda07efe8658419bd98e27acc4788e
KeoBongg <nguyentrongquan27112003@gmail.com>
2024-05-29 13:24:13 +0700

    Adding constructor for model

--- src/main/java/org/booking/bookingapp/model/Booked.java
+++ src/main/java/org/booking/bookingapp/model/Booked.java
@@ -2,16 +2,21 @@ package org.booking.bookingapp.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity(name = "Booked")
 @Table(name = "booked")
 public class Booked {
-    @JsonIgnore
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @EmbeddedId
     private BookedId bookedId;
     @JsonIgnoreProperties("booked")
--- src/main/java/org/booking/bookingapp/model/BookedId.java
+++ src/main/java/org/booking/bookingapp/model/BookedId.java
@@ -2,10 +2,14 @@ package org.booking.bookingapp.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Embeddable
 public class BookedId implements Serializable {
     @Column(name = "user_id")

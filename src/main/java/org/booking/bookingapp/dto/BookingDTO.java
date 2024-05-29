c922f2d513d6ffc8bb158f0b4284f55314759406
KeoBongg <nguyentrongquan27112003@gmail.com>
2024-05-29 16:27:39 +0700

    Update booking service

--- src/main/java/org/booking/bookingapp/dto/BookingDTO.java
+++ src/main/java/org/booking/bookingapp/dto/BookingDTO.java
@@ -2,8 +2,12 @@ package org.booking.bookingapp.dto;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class BookingDTO {
     private Long userId;
     private Long roomId;
+    private LocalDateTime checkIn;
+    private LocalDateTime checkOut;
 }
--- src/main/java/org/booking/bookingapp/service/booking/BookingService.java
+++ src/main/java/org/booking/bookingapp/service/booking/BookingService.java
@@ -2,6 +2,7 @@ package org.booking.bookingapp.service.booking;
 
 import lombok.AllArgsConstructor;
 import org.booking.bookingapp.dto.BookingDTO;
+import org.booking.bookingapp.exception.ApiRequestException;
 import org.booking.bookingapp.exception.NotFoundException;
 import org.booking.bookingapp.model.Booked;
 import org.booking.bookingapp.model.BookedId;
@@ -30,12 +31,34 @@ public class BookingService implements IBookingService{
     }
     @Override
     public Booked bookingRoom(BookingDTO booked) {
+        if(booked.getCheckIn().isBefore(LocalDateTime.now())){
+            throw new ApiRequestException("You must check in after the present time!");
+        }
+        if(booked.getCheckIn().isAfter(booked.getCheckOut())){
+            throw new ApiRequestException("You must set time check in is before time check out");
+        }
+
+        boolean roomAvailable = bookingRepository.findAll().stream()
+                .filter(book -> book.getRooms().getRoomId().equals(booked.getRoomId()))
+                .noneMatch(book ->
+                        (book.getTimeCheckIn().isBefore(booked.getCheckIn()) && book.getTimeCheckOut().isAfter(booked.getCheckIn())) ||
+                        (book.getTimeCheckIn().isBefore(booked.getCheckOut()) && book.getTimeCheckOut().isAfter(booked.getCheckOut())) ||
+                        (book.getTimeCheckIn().isAfter(booked.getCheckIn()) && book.getTimeCheckOut().isBefore(booked.getCheckOut()))
+                );
+
+        if (!roomAvailable) {
+            throw new ApiRequestException("Room has been booked");
+        }
+
         Booked booking = new Booked();
         booking.setBookedId(new BookedId(booked.getUserId(),booked.getRoomId()));
         booking.setUser(usersRepository.findById(booked.getUserId()).orElseThrow(()-> new NotFoundException("The user with id " + booked.getUserId() + " doesn't exist")));
         booking.setRooms(roomsRepository.findById(booked.getRoomId()).orElseThrow(()-> new NotFoundException("Cannot find the room with id " + booked.getRoomId())));
         booking.setCreatedAt(LocalDateTime.now());
+        booking.setTimeCheckIn(booked.getCheckIn());
+        booking.setTimeCheckOut(booked.getCheckOut());
         booking.setResponseStatus(1);
+
         return bookingRepository.save(booking);
     }
 }

4bf1d4eeb942290b535cfa5c78a5eecb4920d0e8
KeoBongg <nguyentrongquan27112003@gmail.com>
2024-06-24 11:25:59 +0700

    create dto for api edit

---
+++ src/main/java/org/booking/bookingapp/request/EditCustomerDTO.java
@@ -0,0 +1,16 @@
+package org.booking.bookingapp.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EditCustomerDTO {
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String phoneNumber;
+    private String avatar;
+}
